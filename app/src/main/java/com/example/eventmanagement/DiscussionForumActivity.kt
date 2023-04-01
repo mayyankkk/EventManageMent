fd99447f5ffa1f49749e37a821c4ae9cf1ca28d0
Mayank Pradhan <nerdmyk@gmail.com>
2023-04-02 01:30:13 +0530

    bug fixes

--- app/src/main/java/com/example/eventmanagement/DiscussionForumActivity.kt
+++ app/src/main/java/com/example/eventmanagement/DiscussionForumActivity.kt
@@ -1,31 +1,43 @@
 package com.example.eventmanagement
 
+import android.annotation.SuppressLint
+import android.app.Dialog
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkInfo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Message
 import android.util.Log
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.eventmanagement.databinding.ActivityDiscussionForumBinding
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.database.*
 import com.google.firebase.database.ktx.database
-import com.google.firebase.database.ktx.getValue
-import com.google.firebase.database.ktx.snapshots
 import com.google.firebase.ktx.Firebase
 
 class DiscussionForumActivity : AppCompatActivity() {
     private lateinit var binding:ActivityDiscussionForumBinding
     lateinit var messages: ArrayList<MSG>
     private lateinit var myAdapter:MyAdapter
+    private var mProgressDialog:Dialog?=null
     private lateinit var databaseReference:DatabaseReference
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         binding= ActivityDiscussionForumBinding.inflate(layoutInflater)
+
+        if(!isInternetConnected()){
+            Toast.makeText(this,"Please Connect To the internet",Toast.LENGTH_SHORT).show()
+            finish()
+        }
+
         setContentView(binding.root)
         val email=Firebase.auth.currentUser?.email.toString()
         val user= email.dropLast(10)
         Log.i("MAYANK",user)
-
+        showProgressBar()
         databaseReference=FirebaseDatabase.getInstance().reference.child("Messages")
 
 
@@ -34,15 +46,18 @@ class DiscussionForumActivity : AppCompatActivity() {
 
 
         databaseReference.addValueEventListener(object : ValueEventListener{
+            @SuppressLint("NotifyDataSetChanged")
             override fun onDataChange(snapshot: DataSnapshot) {
                 Log.i("Mayank",snapshot.toString())
                 messages.clear()
+                dismissProgressBar()
                 for(index in snapshot.children){
                     val msg= MSG(index.child("name").value.toString(),index.child("msg").value.toString())
                     Log.i("Mayank",msg.toString())
                     messages.add(msg)
 
                 }
+                myAdapter.notifyDataSetChanged()
                 binding.etmessage.text.clear()
             }
 
@@ -67,6 +82,21 @@ class DiscussionForumActivity : AppCompatActivity() {
 
 
 
+    }
+    private fun isInternetConnected(): Boolean {
+        val cm: ConnectivityManager =
+            this.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val activeNetwork: NetworkInfo? = cm.activeNetworkInfo
+        return activeNetwork != null && activeNetwork.isConnectedOrConnecting
+    }
+    private fun showProgressBar(){
+        mProgressDialog= Dialog(this@DiscussionForumActivity)
+        mProgressDialog!!.setContentView(R.layout.progress_bar)
+        mProgressDialog!!.show()
+    }
+
+    private fun dismissProgressBar(){
+        mProgressDialog?.dismiss()
     }
 
      private fun writeNewMessage(msg:MSG){
--- app/src/main/java/com/example/eventmanagement/EventList.kt
+++ app/src/main/java/com/example/eventmanagement/EventList.kt
@@ -1,6 +1,11 @@
 package com.example.eventmanagement
 
+import android.annotation.SuppressLint
+import android.app.Dialog
+import android.content.Context
 import android.content.Intent
+import android.net.ConnectivityManager
+import android.net.NetworkInfo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -11,32 +16,47 @@ import com.example.eventmanagement.databinding.ActivityEventListBinding
 import com.google.firebase.database.*
 
 class EventList : AppCompatActivity() {
-    companion object{
-        const val KEY="com.example.eventmanagement.EventList.KEY"
+    companion object {
+        const val KEY = "com.example.eventmanagement.EventList.KEY"
     }
+
     private lateinit var binding: ActivityEventListBinding
     private lateinit var eventAdapter: EventAdapter
     private lateinit var events: ArrayList<Event>
     private lateinit var dbReference: DatabaseReference
+    private var mProgressDialog: Dialog? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding= ActivityEventListBinding.inflate(layoutInflater)
+        binding = ActivityEventListBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        dbReference=FirebaseDatabase.getInstance().reference.child("Event_List")
-        events= arrayListOf()
+        if(!isInternetConnected()){
+            Toast.makeText(this,"Please Connect To the internet",Toast.LENGTH_SHORT).show()
+            finish()
+        }
 
-        dbReference.addValueEventListener(object :ValueEventListener{
+
+        showProgressBar()
+        dbReference = FirebaseDatabase.getInstance().reference.child("Event_List")
+        events = arrayListOf()
+
+        dbReference.addValueEventListener(object : ValueEventListener {
+            @SuppressLint("NotifyDataSetChanged")
             override fun onDataChange(snapshot: DataSnapshot) {
                 events.clear()
-                for(index in snapshot.children){
-                    val event= Event(index.child("name").value.toString(),index.child("num").value.toString())
+                dismissProgressBar()
+                for (index in snapshot.children) {
+                    val event = Event(
+                        index.child("name").value.toString(),
+                        index.child("num").value.toString()
+                    )
                     events.add(event)
                 }
+                eventAdapter.notifyDataSetChanged()
             }
 
             override fun onCancelled(error: DatabaseError) {
-                Toast.makeText(this@EventList,"Database error",Toast.LENGTH_SHORT) .show()
+                Toast.makeText(this@EventList, "Database error", Toast.LENGTH_SHORT).show()
             }
 
         })
@@ -44,9 +64,9 @@ class EventList : AppCompatActivity() {
 
 
 
-        binding.recyclerViewEvents.layoutManager=LinearLayoutManager(this)
-        eventAdapter= EventAdapter(events)
-        binding.recyclerViewEvents.adapter=eventAdapter
+        binding.recyclerViewEvents.layoutManager = LinearLayoutManager(this)
+        eventAdapter = EventAdapter(events)
+        binding.recyclerViewEvents.adapter = eventAdapter
 
 
         eventAdapter.setOnClickListener(object : EventAdapter.OnClickListener {
@@ -58,11 +78,25 @@ class EventList : AppCompatActivity() {
 
     }
 
-    public fun startEventDetails(extra: String){
-        val intent= Intent(this,EventDetails:: class.java)
-        intent.putExtra("KEY",extra)
+    public fun startEventDetails(extra: String) {
+        val intent = Intent(this, EventDetails::class.java)
+        intent.putExtra("KEY", extra)
         startActivity(intent)
     }
 
+    private fun showProgressBar() {
+        mProgressDialog = Dialog(this@EventList)
+        mProgressDialog!!.setContentView(R.layout.progress_bar)
+        mProgressDialog!!.show()
+    }
+    private fun isInternetConnected(): Boolean {
+        val cm: ConnectivityManager =
+            this.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val activeNetwork: NetworkInfo? = cm.activeNetworkInfo
+        return activeNetwork != null && activeNetwork.isConnectedOrConnecting
+    }
+    private fun dismissProgressBar() {
+        mProgressDialog?.dismiss()
+    }
 
 }
